3123537907f227d36fd75e14afd36e1d54ef7cdc
Arjun Bajaj <732586+arjunbajaj@users.noreply.github.com>
2025-12-13 01:21:00 +0530

    Bump versions

--- sdk/elixir/mix.exs
+++ sdk/elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule Fostrom.MixProject do
   def project do
     [
       app: :fostrom,
-      version: "0.0.14",
+      version: "0.0.15",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       compilers: Mix.compilers() ++ [:download_agent],
